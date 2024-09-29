2c031d12159e2f32d9b1ae43ba3379af0180dd5d
Jawad Ali <jawad.ali@repairdesk.co>
2024-09-29 22:38:32 +0500

    next theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "../components/ui/button"
+
 
 export default function Home() {
   return (
-    <div className="">
-      <Button>hit mi</Button>
-    </div>
+    <div>home page</div>
   );
 }
