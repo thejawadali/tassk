a6ca710ea123259ba0d9be0984051333f4022247
Jawad Ali <jawad.ali@repairdesk.co>
2024-09-30 10:10:55 +0500

    make essential changes in input field for validation in base component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,38 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { Label } from "./label";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  touched?: boolean;
+  error?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const baseClass = `flex h-9 w-full rounded-md border bg-transparent px-3 focus-visible:outline-none py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 ${
+      className
+    } ${
+      props.error && props.touched ? "border-red-500" : "border-input focus-visible:ring-1 focus-visible:ring-ring"
+    }`;
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
+      <div>
+        {props.label && <Label htmlFor={props.id}>{props.label}</Label>}
+        <input
+          type={type}
+          className={baseClass}
+          ref={ref}
+          {...props}
+        />
+        {props.touched && props.error ? (
+        <span className="text-red-500 text-sm">{props.error}</span>
+      ) : null}
+      </div>
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
