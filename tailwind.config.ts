30e5aa4495d69b6ec408f26c6fd7bfe9979a3000
Jawad Ali <jawad.ali@repairdesk.co>
2024-10-03 16:21:05 +0500

    shadcn dark theme issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
 
 const config: Config = {
-    darkMode: ["class"],
+    darkMode: "class",
     content: [
+    "**/*.{ts,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -61,6 +63,6 @@ const config: Config = {
       },
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 };
 export default config;
