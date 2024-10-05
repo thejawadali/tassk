ceac225edba85e41038a5e8ca8683f6c7d87b58d
Jawad Ali <jawad.ali@repairdesk.co>
2024-10-05 10:45:13 +0500

    theme changes

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -10,7 +10,7 @@ export default function NavItem({ active, href, label, icon }: { active?: boolea
       {icon}
       {label}
       {active && (
-        <div className="absolute w-1 inset-y-0 -right-[1px] bg-primary rounded-l" />
+        <div className="absolute w-1 inset-y-0 -right-[1px] bg-teal-600 dark:bg-teal-500 rounded-l" />
       )}
     </Link>
   );
